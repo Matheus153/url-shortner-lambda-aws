0c0bf2d1beb94eeb283fe8bb09f5426785ba2cda
Matheus153 <santiosmatheus@gmail.com>
2024-11-20 23:41:27 -0400

    first commit

---
+++ src/main/java/com/rocketseat/createUrlShorter/UrlData.java
@@ -0,0 +1,18 @@
+package com.rocketseat.createUrlShorter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
+public class UrlData {
+
+    private String originalUrl;
+
+    private long expirationTime;
+}
